b1286c204e16eff8032ae815a595e7d2bcf022c8
Jorge Dlg <jdelages@everis.com>
2022-01-27 11:59:20 +0100

    funcion calcular Area Triangulo

--- Triangulo.cs
+++ Triangulo.cs
@@ -2,6 +2,11 @@ namespace TestGit
 {
     public class Triangulo
     {
-        
+        public float CalcArea(float bbase, float altura)
+        {
+            float res = 0;
+            res = (bbase * altura)/2;
+            return  res;
+        }
     }
 }
