1e570118fc82d5431ac740b4500813ce9ff8d434
Carmen Rojo Gosalbez <carmen.rojo.gosalbez@nttdata.com>
2022-01-27 12:01:17 +0100

    area del circulo

--- Clases/Circulo.cs
+++ Clases/Circulo.cs
@@ -6,5 +6,20 @@ namespace TestGit.Clases
 {
     class Circulo
     {
+        private float radio;
+
+        public Circulo(float radio)
+        {
+            this.Radio = radio;
+        }
+
+        protected float Radio { get => radio; set => radio = value; }
+
+        public float Area_Circulo (float radio)
+        {
+            return radio * (float)3.14;
+        }
+
+
     }
 }
--- Clases/Triangulo.cs
+++ Clases/Triangulo.cs
@@ -6,5 +6,18 @@ namespace TestGit.Clases
 {
     class Triangulo
     {
+        private int base_figura;
+        private int altura;
+
+        protected int Base_figura { get => base_figura; set => base_figura = value; }
+        protected int Altura { get => altura; set => altura = value; }
+
+        public Triangulo(int base_figura, int altura)
+        {
+            this.Base_figura = base_figura;
+            this.Altura = altura;
+        }
+
+       
     }
 }
